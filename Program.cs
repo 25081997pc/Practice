d3108d16a3d10a7be630397d48354a0238566d36
25081997pc <chavanpooja2508@gmail.com>
2022-05-21 09:19:34 +0530

    Feature Branch push

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int value1 = 876767;
+            //double value2 = 6.565677;
+            //bool value3 = false;
+            //char value4 = 'p';
+            //string value5 = "pooja";
+
+            //Console.WriteLine("Hello World!!!! - "+ value1);
+            //Console.WriteLine("Double value2 - "+ value2);
+            //Console.WriteLine("Boolean value3 - " + value3);
+            //Console.WriteLine("char value4 - " + value4);
+            //Console.WriteLine("string value5 - " + value5);
+
+            //Console.WriteLine("Please enter the username");
+            //string inputvalue = Console.ReadLine();
+            //Console.WriteLine("Hello " + inputvalue + ", How are you");
+
+
+
+            //Console.WriteLine("Enter the first value");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the second value");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int num3 = num1 + num2;
+
+            //Console.WriteLine("The addition of " + num1 + " and " + num2 + " = " + num3);
+            //Console.WriteLine("Starting of program");
+            //Username.PrinttheUsername();
+            //Console.WriteLine("Ending of program");
+            //Username.Display();
+            //LeapYear.Year();
+
+            //FlipCoin.FlipTheCoin();
+
+            //Console.WriteLine("Please enter the value for number of flip");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //double HeadPercentage = FlipCoin.FlipTheCoin(number, 10);
+            //Console.WriteLine("Head Percentage : " + HeadPercentage);
+            Power.Powerof2();
+
+        }   
+    }
+}    
