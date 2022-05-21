1667b40003ea7b0d88efa8f816e13cd0f496721c
25081997pc <chavanpooja2508@gmail.com>
2022-05-21 09:08:19 +0530

    LeapYear program

---
+++ LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class LeapYear
+    {
+        public static void Year()
+        {
+             int y;
+             Console.WriteLine("Enter the year in four digits : ");
+             y=Convert.ToInt32(Console.ReadLine());
+
+            if((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                Console.WriteLine(" {0} is leap year ", y);
+            }
+
+            else
+            {
+                Console.WriteLine(" {0} is not a leap year ", y);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
